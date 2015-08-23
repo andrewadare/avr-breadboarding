73fabfe2781cd4813fdfe86ef637cee91c60738d
Andrew Adare <andrewadare@gmail.com>
2015-08-23 13:19:02 -0600

    Updated PWM parameters

--- firmware/servo_demo.c
+++ firmware/servo_demo.c
@@ -1,6 +1,6 @@
-// Servo controller example for ATMega*8 at 1MHz.
+// Servo controller example for ATMega*8 at 1MHz or 16 MHz.
 // Servo positions are controlled by (integrated) logic pulses.
-// 20 ms is a typical servo PWM period, and PWM duty cycles are ~1/20-2/20.
+// 20 ms is a typical servo PWM period, and PWM duty cycles are ~ 0.6-2.1 / 20.
 // Hardware setup: servo control wire on PB1.
 
 #include <avr/io.h>
@@ -10,32 +10,49 @@
 #include "USART.h"
 
 // Pulse widths in microseconds. Precise values are servo-dependent.
-#define PULSE_MIN 1000
+#if F_CPU == 16000000UL
+#define PULSE_MIN 1200
+#define PULSE_MID 2850
+#define PULSE_MAX 4600
+#define PWM_MAX   6000
+#else // 1 MHz
+#define PULSE_MIN 600
 #define PULSE_MID 1500
-#define PULSE_MAX 2000
+#define PULSE_MAX 2100
+#define PWM_MAX   3000
+#endif
 
 static inline void init_timer1_servo()
 {
   // Fast PWM (mode 14, table 16-4). TOP (i.e. the PWM counter period) will be
-  // controlled by ICR1, which we set to 20000 (20 ms with CPU/1 prescale).
+  // controlled by ICR1, which we set based on F_CPU to be equivalent to 20 ms.
   TCCR1A |= (1 << WGM11);
   TCCR1B |= ((1 << WGM12) | (1 << WGM13));
-  ICR1 = 20000;
 
-  // Clock/1 prescaling (table 16-5). 1 MHz --> 1 counter tick = 1 us.
+#if F_CPU == 16000000UL
+  // Use clock/8 prescaling for a 16 MHz CPU (table 16-5).
+  // Each counter tick = 0.5 us.
+  // (/16 prescale not available, Ideally that's what I'd use.)
+  TCCR1B |= (1 << CS11);
+  ICR1 = 40000;
+#else
+  // Assume CPU is running at 1 MHz; prescale to clock/1.
+  // Then 1 counter tick = 1 us.
   TCCR1B |= (1 << CS10);
+  ICR1 = 20000;
+#endif
 
   // Send timer output to OC1A, overtaking PB1 (table 16-2, non-inverting mode).
   // In other words, the PWM pulses will be physically output to PB1, and the
   // pulse width is controlled by OCR1A.
   TCCR1A |= (1 << COM1A1);
-
-  // Enable PB1 for output
-  DDRB |= (1 << PB1);
 }
 
 static inline void exercise_servo(void)
 {
+  // Enable PB1 for output
+  DDRB |= (1 << PB1);
+
   printString("Center\r\n");
   OCR1A = PULSE_MID;
   _delay_ms(1500);
@@ -51,6 +68,9 @@ static inline void exercise_servo(void)
   printString("Center\r\n");
   OCR1A = PULSE_MID;
   _delay_ms(1500);
+
+  // Disable PB1 until a new value is entered
+  DDRB &= ~(1 << PB1);
 }
 
 static inline uint16_t serial_atoi16()
@@ -83,11 +103,6 @@ static inline uint16_t serial_atoi16()
 
 int main()
 {
-
-#if (F_CPU == 16000000UL)
-  clock_prescale_set(clock_div_16);
-#endif
-
   uint16_t pulse_width;
 
   // Home the servo
@@ -107,17 +122,15 @@ int main()
     printString("Slewing....\r\n");
     OCR1A = pulse_width;
 
-    // Enable PB1 for output during high part of PWM cycle
+    // Enable PB1 for output
     DDRB |= (1 << PB1);
 
     _delay_ms(1000);
 
-    printString("Releasing...\r\n");
-
     // Delay while pulse is high, + margin
-    while (TCNT1 < 3000) {;}
+    while (TCNT1 < PWM_MAX) {;}
 
-    // Disable PB1 during low part of PWM cycle.
+    // Disable PB1 until a new value is entered
     DDRB &= ~(1 << PB1);
   }
 
