5066bcac6bf78e2babeeb66a5ce7688924caccaf
Andrew Adare <andrewadare@gmail.com>
2015-08-31 22:13:22 -0600

    Found and defined line start positions, make lcd_goto work correctly.

--- firmware/i2c_lcd.c
+++ firmware/i2c_lcd.c
@@ -1,5 +1,4 @@
 // I2C code for AVR master and mcp23008 port expander slave
-// Based on blog post at http://www.ermicro.com/blog/?p=1239
 
 // For use with the Adafruit I2C/SPI backpack for LCDs using the Hitachi
 // HD44780 driver or equivalent. Uses 4 bit mode. Only I2C implemented here.
@@ -17,12 +16,11 @@
 #define LCD_DB7    6 //        
 #define LCD_LITE   7 // GP7:   LCD backlight on/off (only on 16 pin LCDs)
 
-#define LCD_DDRAM  LCD_DB7 // bit for setting DDRAM address
-
-// #define LINE1 0x00    // First char on line 1
-// #define LINE2 0x40    // First char on line 2
-// #define LINE3 0x14    // First char on line 3
-// #define LINE4 0x54    // First char on line 4
+// Line start positions for a 20x4 display
+#define LINE1  0 // First char on line 1
+#define LINE2 40 // First char on line 2
+#define LINE3 20 // First char on line 3
+#define LINE4 84 // First char on line 4
 
 void send_nibble(uint8_t rs, uint8_t nibble, uint8_t backlit)
 {
@@ -48,12 +46,12 @@ void send_nibble(uint8_t rs, uint8_t nibble, uint8_t backlit)
   // Set enable bit high and write
   byte |= (1 << LCD_E);
   write_mcp23008(MCP23008_GPIO, byte);
-  _delay_ms(1);
+  _delay_us(200);
 
   // Set enable bit low and write the same data again
   byte &= ~(1 << LCD_E);
   write_mcp23008(MCP23008_GPIO, byte);
-  _delay_ms(1);
+  _delay_us(200);
 }
 
 // Using LCD in 4 bit mode requires writing the high and low nibbles
@@ -109,19 +107,22 @@ void lcd_puts(const char *s, uint8_t backlit)
 
 void lcd_goto(uint8_t pos)
 {
-  lcd_command((1 << LCD_DDRAM) + pos);
+  // This implements the "Set DDRAM address" instruction.
+  // The HD44780 is designed to control a 4 x 40 display, and the resulting
+  // position depends on your LCD dimensions.
+  // Best to find and define LINE1-LINE4, then navigate w.r.t those points.
+  send_byte(0, 0x80 + pos, 1);
 }
 
 void lcd_clrscr()
 {
-  send_nibble(0, 0, 1);
-  send_nibble(0, 1, 1);
+  send_byte(0, 0x01, 1);
 }
 
 void lcd_home()
 {
-  send_nibble(0, 0, 1);
-  send_nibble(0, 2, 1);
+  send_byte(0, 0x02, 1);
+  _delay_ms(2);
 }
 
 int main()
@@ -133,36 +134,30 @@ int main()
   while (1)
   {
     lcd_clrscr();
-    // lcd_home();
 
-    // lcd_goto(LINE1);
-    // lcd_puts("Line 1", backlit);
-    // _delay_ms(500);
+    lcd_goto(LINE1);
+    lcd_puts("Line 1", backlit);
 
-    // lcd_goto(LINE2);
-    // lcd_puts("Line 2", backlit);
-    // _delay_ms(500);
+    lcd_goto(LINE2);
+    lcd_puts("Line 2", backlit);
 
-    // lcd_goto(LINE3);
-    // lcd_puts("Line 3", backlit);
-    // _delay_ms(500);
+    lcd_goto(LINE3);
+    lcd_puts("Line 3", backlit);
 
-    // lcd_goto(LINE4);
-    // lcd_puts("Line 4", backlit);
-    // _delay_ms(500);
+    lcd_goto(LINE4);
+    lcd_puts("Line 4", backlit);
 
+    _delay_ms(500);
 
-    for (uint8_t i = 0; i < 5; i++)
+    // Blink the backlight
+    for (uint8_t i=0; i<6; i++)
     {
-      if (i == 0)
-      {
-        lcd_clrscr();
-        lcd_home();
-      }
-
-      lcd_puts("Hello World!", backlit);
-      _delay_ms(500);
+      send_byte(0, 0x80 + LINE4 + 6, i%2);
+      _delay_ms(50);
     }
+
+    _delay_ms(500);
+
   }
 
   return 0;
