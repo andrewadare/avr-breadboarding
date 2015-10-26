8c360d5c121505cdcfecffa310ae61f9e1a664be
Andrew Adare <andrewadare@gmail.com>
2015-10-26 15:46:05 -0600

    Pass in a single byte for I2C address.

--- common/MCP23008.c
+++ common/MCP23008.c
@@ -1,29 +1,35 @@
 #include "MCP23008.h"
 
-void write_mcp23008(unsigned char reg_addr,unsigned char data)
+void write_mcp23008(uint8_t reg_addr, uint8_t data)
 {
-  // Start the I2C Write Transmission
-  i2c_start(MCP23008_ID, MCP23008_ADDR, TW_WRITE);
-  // Sending the Register Address
+  // Start the I2C write transmission
+  i2c_start(MCP23008, TW_WRITE);
+
+  // Then send register address
   i2c_write(reg_addr);
+
   // Write data to MCP23008 Register
   i2c_write(data);
+
   // Stop I2C Transmission
   i2c_stop();
 }
 
-unsigned char read_mcp23008(unsigned char reg_addr)
+uint8_t read_mcp23008(uint8_t reg_addr)
 {
   char data;
-  // Start the I2C Write Transmission
-  i2c_start(MCP23008_ID, MCP23008_ADDR, TW_WRITE);
-  // Read data from MCP23008 Register Address
+
+  // Start the I2C write transmission
+  i2c_start(MCP23008, TW_WRITE);
+
+  // Then send register address
   i2c_write(reg_addr);
+
   // Stop I2C Transmission
   i2c_stop();
 
   // Re-Start the I2C Read Transmission
-  i2c_start(MCP23008_ID, MCP23008_ADDR, TW_READ);
+  i2c_start(MCP23008, TW_READ);
   i2c_read(&data, 0); // 0 for NACK
 
   // Stop I2C Transmission
