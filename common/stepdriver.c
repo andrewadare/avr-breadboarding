cb7f523906b5340af83132aa5d4bcfbea1234d65
Andrew Adare <andrewadare@gmail.com>
2015-12-21 16:13:34 -0700

    Adding stepdriver.h,c to replace A4988driver. Avoids using interrupts or CPU

---
+++ common/stepdriver.c
@@ -0,0 +1,143 @@
+#include "stepdriver.h"
+
+volatile int16_t position = 0;
+
+void stepper_setup()
+{
+  // Enable the control pins for output, but leave them low.
+  STEPPER_DDR |= ((1 << DIR_PIN) | (1 << STEPPER_ENABLE_PIN));
+
+  // Configure timer x to tick with a 4us period, where x is 0 or 2.
+  // Toggling occurs every OCRxA*4 us.
+  // Square waves are generated, so the pulse period is twice the pulse width.
+#ifdef USE_TIMER_0_FOR_STEPPER
+  TCCR0A |= (1 << WGM01);                  // Timer 0 to CTC mode
+  TCCR0B |= ((1 << CS01) | (1 << CS00));   // Prescale to F_CPU/64
+  OCR0A = OCR0B = 255;                     // Init to max (~1ms) pulse width
+  TCCR0A |= (1 << COM0B0);  // Toggle OC0B/PD5 when TCNT0 == OCR0B. Table 15-5
+  TCCR0A |= (1 << COM0A0);  // Toggle OC0A/PD6 when TCNT0 == OCR0A. Table 15-2
+#endif
+
+#ifdef USE_TIMER_2_FOR_STEPPER
+  TCCR2A |= (1 << WGM21);                  // Timer 2 to CTC mode
+  TCCR2B |= (1 << CS22);                   // Prescale to F_CPU/64
+  OCR2A = OCR2B = 255;                     // Init to max (~1ms) pulse width
+  // TCCR2A |= (1 << COM2A0);  // Toggle OC2A/PB3 when TCNT2 == OCR2A. Table 18-2
+  TCCR2A |= (1 << COM2B0);  // Toggle OC2B/PD3 when TCNT2 == OCR2B. Table 18-5
+#endif
+}
+
+void enable_stepper()
+{
+  STEPPER_PORT &= ~(1 << STEPPER_ENABLE_PIN);
+}
+
+void disable_stepper()
+{
+  STEPPER_PORT |= (1 << STEPPER_ENABLE_PIN);
+}
+
+void set_stepper_position(int16_t p)
+{
+  position = p;
+}
+
+int16_t stepper_position()
+{
+  return position;
+}
+
+void setdir_cw()
+{
+  STEPPER_PORT |= (1 << DIR_PIN);
+}
+
+void setdir_ccw()
+{
+  STEPPER_PORT &= ~(1 << DIR_PIN);
+}
+
+// Take n steps with a pulse width (half-period) t
+void step(uint16_t n, uint8_t t)
+{
+  uint16_t step_counter = 0;
+  int8_t one_step = (bit_is_set(STEPPER_PORT, DIR_PIN)) ? 1 : -1;
+
+  // Set pin toggle rate to max(t,1).
+#ifdef USE_TIMER_0_FOR_STEPPER
+  OCR0A = t > 1 ? t : 1;
+  OCR0B = OCR0A;
+#endif
+#ifdef USE_TIMER_2_FOR_STEPPER
+  OCR2A = t > 1 ? t : 1;
+  OCR2B = OCR2A;
+#endif
+
+  // Stepping is handled by timer output to STEP_PIN (OCxA).
+  // All that is needed here is to enable output, count steps, and disable.
+  STEPPER_DDR |= (1 << STEP_PIN);
+  while (step_counter < n)
+  {
+    loop_until_bit_is_set(STEPPER_PIN_REG, STEP_PIN);
+    step_counter++;
+    position += one_step;
+    loop_until_bit_is_clear(STEPPER_PIN_REG, STEP_PIN);
+  }
+  STEPPER_DDR &= ~(1 << STEP_PIN);
+}
+
+void ramp_move(
+  uint16_t n,     // Number of steps
+  uint8_t tmax,   // Pulse width at start and end of ramp (thus maximum)
+  uint8_t tmin,   // Pulse width at max speed (thus minimum)
+  uint8_t nramp)  // Number of steps in ramp interval
+{
+  uint8_t t = tmax;
+  uint8_t dt = (t - tmin) / nramp;
+  uint16_t steps_taken = 0;
+
+  // Trapezoid-like ramp schedule
+  if (n > 2*nramp)
+  {
+    // Ramp up
+    while (steps_taken < nramp)
+    {
+      step(1, t);
+      t -= dt;
+      steps_taken++;
+    }
+
+    // Constant (max) speed
+    t = tmin;
+    step(n - 2*nramp, t);
+    steps_taken += n - 2*nramp;
+
+    // Ramp down
+    while (steps_taken < n)
+    {
+      step(1, t);
+      t += dt;
+      steps_taken++;
+    }
+  }
+  // Truncated (pyramid-like) ramp schedule for short moves
+  else
+  {
+    // Ramp up
+    while (steps_taken < n/2)
+    {
+      step(1, t);
+      t -= dt;
+      steps_taken++;
+    }
+
+    // Ramp down
+    t += dt;
+    while (steps_taken < n)
+    {
+      step(1, t);
+      t += dt;
+      steps_taken++;
+    }
+  }
+}
---
+++ common/stepdriver.h
@@ -0,0 +1,49 @@
+// Code to control stepper drivers using the Allegro A4988, TI DRV8825, or 
+// equivalent. This involves controlling 3 pins: direction, step, and enable.
+
+#ifndef __STEPDRIVER_H__
+#define __STEPDRIVER_H__
+#include <avr/io.h>
+
+void stepper_setup();
+void enable_stepper();
+void disable_stepper();
+void setdir_cw();
+void setdir_ccw();
+void step(uint16_t n, uint8_t t);
+void set_stepper_position(int16_t p);
+int16_t stepper_position();
+
+/*
+// Timer 0 can output pulses to PD5 and/or PD6 without CPU or interrupts.
+// So only PORTD can be used.
+#define USE_TIMER_0_FOR_STEPPER 1
+#define STEPPER_PORT            PORTD      // Must be D
+#define STEPPER_DDR             DDRD       //  "
+#define STEPPER_PIN_REG         PIND
+#define STEP_PIN                5          // Must be 5 or 6
+#define DIR_PIN                 4
+#define STEPPER_ENABLE_PIN      3
+*/
+
+// Timer 2 can pulse to PB3 and/or PD3 without CPU or interrupts. Thus either
+// PORTB or PORTD can be used, but STEP_PIN must remain 3.
+#define USE_TIMER_2_FOR_STEPPER 1
+#define STEPPER_PORT            PORTD
+#define STEPPER_DDR             DDRD
+#define STEPPER_PIN_REG         PIND
+#define STEP_PIN                3          // Must be 3
+#define DIR_PIN                 4
+#define STEPPER_ENABLE_PIN      5
+
+// Take n steps at a speed controlled by the provided ramp params (esp. tmin).
+// Some options for tmax,tmin,nramp triplets, from fastest to slowest:
+// 255,75,18; 240,80,16; 250,100,15
+// Currently using 240,80,16
+void ramp_move(
+       uint16_t n,     // Number of steps
+       uint8_t tmax,   // Pulse width at start and end of ramp (thus maximum)
+       uint8_t tmin,   // Pulse width at max speed (thus minimum)
+       uint8_t nramp); // Number of steps in ramp interval
+
+#endif  /* __STEPDRIVER_H__ */
